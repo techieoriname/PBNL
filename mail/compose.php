0a86a65ad8005bc22349053a370228003334654d
Techie Oriname <33547272+techieoriname@users.noreply.github.com>
2019-01-09 18:14:16 +0100

    Update compose.php

--- mail/compose.php
+++ mail/compose.php
@@ -1,21 +1,7 @@
 <?php
 ERROR_REPORTING(0);
 require("../init.php");
-/*
-Project: PBNL Forum v2.3
 
-Coded By: Oyebamiji Akorede (Harkorede)
-
-Facebook: http://facebook.com/harkorede94
-
-Email: (harkorede01@gmail.com)
-
-CellPhone +2348029828364
-
-Twitter: @harkorede94
-
-WebSite: http://naijamobile.in
-*/
 if(!isloggedin())
 { header("location: ../index.php"); }
 else {
